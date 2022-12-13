60ff8c3e596696c84334a2f909dd0b3301ddb8ba
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2022-12-14 00:51:02 +0900

    sessions_helper編集

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,4 +5,10 @@ module SessionsHelper
   def logged_in?
     current_user.present?
   end
+
+  def destroy
+    session.delete(:uder_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
 end
