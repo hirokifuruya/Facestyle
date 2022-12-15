87b4f0544ee0987b21a1ec7c69eb5913c7b00d53
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2022-12-15 10:48:21 +0900

    controllersファイル作成

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,15 @@
 module SessionsHelper
-  def surrent_user
+  def current_user
     @current_user ||= User.find_by(id: session[:user_id])
-  end
-  def logged_in?
-    current_user.present?
   end
 
   def destroy
     session.delete(:uder_id)
     flash[:notice] = 'ログアウトしました'
     redirect_to new_session_path
+  end
+
+  def logged_in?
+    current_user.present?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_13_021404) do
+ActiveRecord::Schema.define(version: 2022_12_13_160848) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +30,5 @@ ActiveRecord::Schema.define(version: 2022_12_13_021404) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "blogs", "users"
 end
