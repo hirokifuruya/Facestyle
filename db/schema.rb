49b782a050396bbb1e91ed2f904d89b40e474f92
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2022-12-16 11:26:58 +0900

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2022_12_15_091310) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "image"
